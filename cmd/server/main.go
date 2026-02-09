b6b9ae0f0029f75ff8f1b4a5729671bb56b2146a
Fahmi Adi Nugraha <fnugraha@gmail.com>
2026-02-09 14:38:58 +0700

    Add code to 'main' that connects to an existing instance of RabbitMQ

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	serverUrl := "amqp://guest:guest@localhost:5672/"
+	serverConn, err := amqp.Dial(serverUrl)
+	if err != nil {
+		log.Fatalf("Error connecting to server: %s\n", err)
+	}
+	defer serverConn.Close()
+	fmt.Println("Connected to Peril server")
+
+	exit := make(chan os.Signal, 1)
+	signal.Notify(exit, syscall.SIGINT, syscall.SIGTERM)
+	<-exit
+
+	fmt.Println("Shutting down...")
+	err = serverConn.Close()
+	if err != nil {
+		log.Fatalf("Could not properly shut down server: %s\n", err)
+	}
+	fmt.Println("Server has successfully shut down.")
 }
