34805cc98bfc1b052f83a1a26b58659f84a7eea8
Fahmi Adi Nugraha <fnugraha@gmail.com>
2026-02-10 10:02:07 +0700

    Add 'PublishJSON' function for publishing event via channel and publish pause event

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,6 +7,8 @@ import (
 	"os/signal"
 	"syscall"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -16,10 +18,21 @@ func main() {
 	serverUrl := "amqp://guest:guest@localhost:5672/"
 	serverConn, err := amqp.Dial(serverUrl)
 	if err != nil {
-		log.Fatalf("Error connecting to server: %s\n", err)
+		log.Fatalf("Error connecting to server: %s\n", err.Error())
 	}
 	defer serverConn.Close()
 	fmt.Println("Connected to Peril server")
+
+	serverChan, err := serverConn.Channel()
+	if err != nil {
+		log.Fatalf("Error creating channel over server connection: %s\n", err.Error())
+	}
+
+	perilPlayingState := routing.PlayingState{IsPaused: true}
+	err = pubsub.PublishJSON(serverChan, routing.ExchangePerilDirect, routing.PauseKey, perilPlayingState)
+	if err != nil {
+		log.Fatalf("Error pubishing pause state: %s\n", err.Error())
+	}
 
 	exit := make(chan os.Signal, 1)
 	signal.Notify(exit, syscall.SIGINT, syscall.SIGTERM)
@@ -28,7 +41,7 @@ func main() {
 	fmt.Println("Shutting down...")
 	err = serverConn.Close()
 	if err != nil {
-		log.Fatalf("Could not properly shut down server: %s\n", err)
+		log.Fatalf("Could not properly shut down server: %s\n", err.Error())
 	}
 	fmt.Println("Server has successfully shut down.")
 }
---
+++ internal/pubsub/pubsub.go
@@ -0,0 +1,32 @@
+package pubsub
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
+	valBytes, err := json.Marshal(val)
+	if err != nil {
+		return fmt.Errorf("marshaling error: %w", err)
+	}
+
+	err = ch.PublishWithContext(
+		context.Background(),
+		exchange,
+		key,
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "application/json",
+			Body:        valBytes,
+		})
+	if err != nil {
+		return fmt.Errorf("publishing error: %w", err)
+	}
+
+	return nil
+}
